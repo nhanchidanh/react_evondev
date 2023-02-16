fabd361f70da97788e85f7c891549fa710039daa
nhanchidanh <nhanchidanh@gmail.com>
2023-02-17 00:15:29 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import Game from "./components/tictactoe/Game";
+import Photos from "./components/photo/Photos";
 
 function App() {
   return (
     <div className="App">
-      <Game></Game>
-      <h1 className="text-3xl font-bold underlinec text-[100px]">
-        Hello world!
-      </h1>
+      <Photos></Photos>
     </div>
   );
 }
